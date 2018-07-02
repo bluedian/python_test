848c33cb7d21c2ee2b6eefbcb4a1cfc6845b477e
Administrator <6453776@qq.com>
2018-07-02 16:09:13 +0800

    a1

--- unit_redis/redis_bese.py
+++ unit_redis/redis_bese.py
@@ -9,3 +9,10 @@ r.set('foo', 'Bar1')
 r.set('foo', 'Bar2')
 
 print(r.get('foo'))
+
+r.set('foo1', 'Bar1')
+r.set('foo2', 'Bar2')
+r.set('foo3', 'Bar3')
+r.set('foo4', 'Bar4')
+
+print(str(r.get('foo1')))
