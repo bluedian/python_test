094dd91b023cc5f008c354d7b17ae75266bbed6f
Administrator <6453776@qq.com>
2018-07-02 17:42:36 +0800

    a1

--- unit_redis/redis_request_w.py
+++ unit_redis/redis_request_w.py
@@ -11,8 +11,11 @@ def redis_list(data_name,data):
 
 url='http://oa.9oe.com/index.php/book/api'
 html=requests.get(url).text
+
 print(html)
+print(type(html))
 json_html=json.loads(html)
+print(type(json_html))
 for item in json_html['data']:
     print(item)
     print(type(item))
