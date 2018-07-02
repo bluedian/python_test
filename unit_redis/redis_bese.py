67ebb388721526f7d7eff4354a98510879f87e89
Administrator <6453776@qq.com>
2018-07-02 16:29:04 +0800

    a3

--- unit_redis/redis_bese.py
+++ unit_redis/redis_bese.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import redis
+import json
 
 r = redis.Redis(host='127.0.0.1', port=6379)
 r.set('foo', 'Bar')
@@ -20,3 +21,20 @@ r.set('foo4', abc)
 print(str(r.get('foo1')))
 print(type(r.get('foo2')))
 print(r.get('foo4'))
+
+data={
+    'a1':'a1111',
+    'a2':'a2222',
+    'a2':'a3333'
+}
+
+json_str=json.dumps(data)
+
+r.set('cccccc', json_str)
+
+re_date= r.get('cccccc')
+print(re_date)
+print(type(re_date))
+u1=re_date.decode('utf-8')
+print(u1)
+print(type(u1))
