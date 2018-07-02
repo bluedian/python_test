ec26b52a946caeeb2ef5d961b30ab773ff649a40
Administrator <6453776@qq.com>
2018-07-02 17:44:35 +0800

    a1

--- unit_redis/redis_request_r.py
+++ unit_redis/redis_request_r.py
@@ -6,15 +6,17 @@ import requests
 
 def redis_list_read(data_name):
     r = redis.Redis(host='127.0.0.1', port=6379)
-    abc=r.lpop(data_name)
+    abc = r.lpop(data_name)
     print(abc)
     print(type(abc))
 
-    abc_dic=abc.decode('utf-8')
-    print('abc_dic-->type-->:',type(abc_dic))
+    abc_dic = abc.decode('utf-8')
+    print('abc_dic-->type-->:', type(abc_dic))
 
     try:
-        abc_json=json.loads(abc_dic)
+        abc_json = json.loads(abc_dic)
+        print(abc_json)
+        print(type(abc_json))
         print(abc_json['name'])
     except:
         print('---------')
