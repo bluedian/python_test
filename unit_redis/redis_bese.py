1842ffede492cb801ee4f3f3f391853d37b0b567
Administrator <6453776@qq.com>
2018-07-02 16:40:31 +0800

    a5

--- unit_redis/redis_bese.py
+++ unit_redis/redis_bese.py
@@ -2,43 +2,56 @@
 import redis
 import json
 
-r = redis.Redis(host='127.0.0.1', port=6379)
-r.set('foo', 'Bar')
-print(r.get('foo'))
+def redis_key():
+    r = redis.Redis(host='127.0.0.1', port=6379)
+    r.set('foo', 'Bar')
+    print(r.get('foo'))
 
-r.set('foo', 'Bar1')
-r.set('foo', 'Bar2')
+    r.set('foo', 'Bar1')
+    r.set('foo', 'Bar2')
 
-print(r.get('foo'))
+    print(r.get('foo'))
 
-r.set('foo1', 'Bar1')
-r.set('foo2', 'Bar2')
-r.set('foo3', 'Bar3')
-abc='string'
+    r.set('foo1', 'Bar1')
+    r.set('foo2', 'Bar2')
+    r.set('foo3', 'Bar3')
+    abc='string'
 
-r.set('foo4', abc)
+    r.set('foo4', abc)
 
-print(str(r.get('foo1')))
-print(type(r.get('foo2')))
-print(r.get('foo4'))
+    print(str(r.get('foo1')))
+    print(type(r.get('foo2')))
+    print(r.get('foo4'))
 
-data={
-    'a1':'a1111',
-    'a2':'a2222',
-    'a2':'a3333'
-}
+    data={
+        'a1':'a1111',
+        'a2':'a2222',
+        'a2':'a3333'
+    }
 
-json_str=json.dumps(data)
+    json_str=json.dumps(data)
 
-r.set('cccccc', json_str)
+    r.set('cccccc', json_str)
 
-re_date= r.get('cccccc')
-print(re_date)
-print(type(re_date))
-u1=re_date.decode('utf-8')
-print(u1)
-print(type(u1))
+    re_date= r.get('cccccc')
+    print(re_date)
+    print(type(re_date))
+    u1=re_date.decode('utf-8')
+    print(u1)
+    print(type(u1))
 
-guo_01=json.loads(u1)
-print('a1:',guo_01['a1'])
+    guo_01=json.loads(u1)
+    print('a1:',guo_01['a1'])
+
+def redis_list():
+    r = redis.Redis(host='127.0.0.1', port=6379)
+
+    for i in range(10):
+        list_num='http://www.baidu.com/wd=abc%d' % (i)
+        r.lpush('list_guo',list_num)
+
+    print(r.lpop('list_guo'))
+    print(type(r.lpop('list_guo')))
+
+redis_list()
 
