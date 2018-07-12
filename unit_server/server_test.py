9ed241ca7061d1b4d8f3e168196c940962b2dabf
Administrator <6453776@qq.com>
2018-07-12 18:24:05 +0800

    a1

--- unit_server/server_test.py
+++ unit_server/server_test.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 
-import sys
+import sys,os
+rootpath=os.path.dirname(os.getcwd())
 syspath=sys.path
-print(syspath)
+sys.path=[]
+sys.path.append(rootpath)#将工程根目录加入到python搜索路径中
+sys.path.extend([rootpath+i for i in os.listdir(rootpath) if i[0]!="."])#将工程目录下的一级目录添加到python搜索路径中
+sys.path.extend(syspath)
+
+print(rootpath)
