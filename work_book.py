1d93ef26e18739450027caadadbba37c1998e0c0
Administrator <6453776@qq.com>
2018-07-16 19:14:04 +0800

    v16

--- work_book.py
+++ work_book.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import requests
 import json
 import re
-
+from urllib.parse import urljoin
 
 
 class work_book():
@@ -25,18 +25,24 @@ class work_book():
 
     def getBookKey(self):
         book_key=''
-
         data={
             'model':'book_key_get',
         }
         req=requests.post(url=self.server_url,data=data)
-        html=req.text
-        json_data=json.loads(html)
+        #html=req.text
+        print(req.text)
+        json_data=req.json()
+        #json_data=json.loads(html)
         book_key=json_data['data']
         print(book_key['book_key'])
         return book_key['book_key']
 
-    def soupLinkAll(self,query_url):
+    def serverSwap(self,data):
+        req=requests.post(url=self.server_url,data=data)
+        html=req.text
+        return html
+
+    def soupLinkAll(self,query_url,book_name='大王饶命'):
         html=self.getHtml(query_url)
         if html is None:
             return
@@ -45,23 +51,70 @@ class work_book():
         a_all = soup.find_all('a')
         for item in a_all:
             text_lower=item.get_text().lower()
-            if text_lower.find('aaaa')>=0:
+            #print(text_lower)
+            if text_lower.find(book_name)>=0:
                 #print(item)
                 print(item.get_text(),item.attrs['href'])
+                self.work_two_1(item.attrs['href'])
 
 
+    def getIndexUrl(self,query_url,index=0):
+        header={
+            'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36',
+        }
+        try:
+            req = requests.get(query_url,headers=header,timeout=2)
+        except:
+            print(query_url)
+            return None,None
+
+        req.encoding = req.apparent_encoding
+        soup = BeautifulSoup(req.text, 'html.parser')
+        title_text=soup.title.get_text()
+        title_url=req.url
+
+        data={
+            'model':'index_url',
+            'index':index,
+            'name':title_url,
+            'title':title_text
+        }
+        print(self.serverSwap(data=data))
+
+        return title_text,title_url
 
 
-    def word_one(self):
-        query_url=self.baidu_url+self.getBookKey()
-        #work_html=self.getHtml(query_url)
+    def work_two_1(self,query_url):
+        book_title, book_url = self.getIndexUrl(query_url)
+        if book_url is None:
+            return
+        bese_url = urljoin(book_url, '/')
+        if bese_url==book_url:
+            return
+        ceshi_title, ceshi_url = work_book().getIndexUrl(bese_url, index=1)
+        print(ceshi_title,ceshi_url)
+        return True
+
+
+    def word_one(self,book_name='大王饶命'):
+        # query_url=self.baidu_url+self.getBookKey()
+        query_url = self.baidu_url + book_name
+        # work_html=self.getHtml(query_url)
         print(query_url)
-        self.soupLinkAll(query_url)
+        self.soupLinkAll(query_url,book_name=book_name)
 
-        #print(work_html)
+    def run(self):
+        print(self.getBookKey())
+        get_book_key=self.getBookKey()
+        self.word_one(get_book_key)
 
 
 
 
 if __name__ == '__main__':
-    work_book().word_one()
+
+    #work_book().word_one()
+    work_book().run()
+
+
+
