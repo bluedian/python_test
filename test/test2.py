8b4b1ff5d09b7806ca077e0fa7e31d8a50beba65
Administrator <6453776@qq.com>
2018-07-18 11:03:17 +0800

    v18.3

--- test/test2.py
+++ test/test2.py
@@ -3,15 +3,15 @@ import sys
 sys.path.append('..')
 print(sys.path)
 
-#from test.test1 import test1
-import test.test1
+from test.test1 import test1
+#import test.test1
 
 class test2(object):
     def __init__(self):
         pass
 
     def tt(self):
-        t1=test.test1()
+        t1=test1()
         t1.pp()
 
 
