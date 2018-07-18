56e05bd81108891bf4c7e6c80aa9a62ba946f77c
Administrator <6453776@qq.com>
2018-07-18 11:09:15 +0800

    v18.4

---
+++ test/test4.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import test1
+
+class test4(object):
+    def __init__(self):
+        pass
+
+    def tt(self):
+        t1=test1()
+        t1.pp()
+
+
+if __name__ == '__main__':
+    test4().tt()
