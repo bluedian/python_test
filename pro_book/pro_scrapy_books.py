15bd2a490c09174bfd8783a04596822d9860c482
Administrator <6453776@qq.com>
2018-08-04 12:07:09 +0800

    v8.4.8

--- pro_book/pro_scrapy_books.py
+++ pro_book/pro_scrapy_books.py
@@ -1,28 +1,27 @@
 # -*- coding: utf-8 -*-
-import sys, os
+import os, sys
+
 sys.path.append('..')
 
-import requests
-import socket
-
-import bese_config
 import bese_crontab
 
-class server_status():
+
+class pro_scrapy_books():
+    '''
+    工程使用模板
+    '''
+
     def __init__(self):
         '''
         初始化
         '''
-        self.indexName = 'server_status.py'
-        self.version = '0.0.3'
+        self.indexName = 'pro_scrapy_books.py'
+        self.version = '0.0.1'
         self.indexPath = os.path.abspath(os.path.dirname(__file__))
         self.indexPathName = self.indexPath + '/' + self.indexName
 
-        self.indexCronComt = 'gsjob_server_status'
-        self.indexCronTime = '*/3 * * * *'
-
-        self.hostName = 'book_server'
-        # self.serverUrl = 'http://oa.9oe.com/index.php/book/apiserver'
+        self.indexCronComt = 'gsjob_pro_scrapy_books'
+        self.indexCronTime = '*/2 * * * *'
 
     def info(self):
         '''
@@ -34,7 +33,7 @@ class server_status():
         print('indexCronTime:', self.indexCronTime)
         print('indexCronComt:', self.indexCronComt)
 
-    def init_cron(self):
+    def init_cron(self, setM=2):
         '''
         增加类的时间任务执
         :return:
@@ -43,7 +42,7 @@ class server_status():
         commond = 'python3 ' + self.indexPathName
         comment = self.indexCronComt
         cron = bese_crontab.bese_crontab()
-        cron.appendCron(setmin=2, comd=commond, comt=comment)
+        cron.appendCron(setmin=setM, comd=commond, comt=comment)
 
     def del_cron(self):
         '''
@@ -54,38 +53,17 @@ class server_status():
         cron = bese_crontab.bese_crontab()
         cron.delCron(comt=self.indexCronComt)
 
-    def get_hostname(self):
-        try:
-            self.hostName = socket.gethostname()
-        except:
-            pass
-        return self.hostName
-
-    def update_status(self):
-        try:
-            urlServer = bese_config.urlServer
-        except:
-            pass
-        data = {
-            'model': 'status',
-            'server_hostname': self.get_hostname(),
-            'server_version': self.version,
-        }
-        html = requests.post(url=urlServer, data=data).text
-        print(html)
-
-    def update_git(self):
-        print(os.system("ls"))
-
-
     def run(self):
-        print(bese_config.urlServer)
-        self.update_status()
+        '''
+        任务运行主函数
+        :return:
+        '''
+        print('run')
+        pass
 
 
 if __name__ == '__main__':
-    gs_class_self = server_status()
-
+    gs_class_self = pro_moban()
     if len(sys.argv) == 2:
         if sys.argv[1] == 'init':
             gs_class_self.init_cron()
@@ -93,7 +71,5 @@ if __name__ == '__main__':
             gs_class_self.info()
         if sys.argv[1] == 'del':
             gs_class_self.del_cron()
-        if sys.argv[1] == 'up':
-            gs_class_self.update_git()
     else:
         gs_class_self.run()
